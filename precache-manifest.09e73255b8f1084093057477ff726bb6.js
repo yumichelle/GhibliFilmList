d4216a539f81c2a30e80e5fedd59ba5d6907a9d1
michelle yu <y.michelle03@gmail.com>
2020-12-26 19:38:13 -0500

    Updates

---
+++ precache-manifest.09e73255b8f1084093057477ff726bb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "217398c583ef8e0fb293",
+    "url": "/GhibliFilmList/static/css/main.8957cf14.chunk.css"
+  },
+  {
+    "revision": "217398c583ef8e0fb293",
+    "url": "/GhibliFilmList/static/js/main.217398c5.chunk.js"
+  },
+  {
+    "revision": "85c5185a5a876a04fd7f",
+    "url": "/GhibliFilmList/static/js/1.85c5185a.chunk.js"
+  },
+  {
+    "revision": "c4a419c290e008b1bec3",
+    "url": "/GhibliFilmList/static/js/runtime~main.c4a419c2.js"
+  },
+  {
+    "revision": "881c8060bf86a139f776a5b40c86d13f",
+    "url": "/GhibliFilmList/index.html"
+  }
+];
